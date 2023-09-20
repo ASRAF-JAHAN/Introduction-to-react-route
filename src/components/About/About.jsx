3d95263737f735a6df20d321668d48864c5e4b91
ASRAF JAHAN <webasraf3@gmail.com>
2023-09-20 22:15:42 +0600

    first commit

---
+++ src/components/About/About.jsx
@@ -0,0 +1,12 @@
+
+
+const About = () => {
+    return (
+        <div>
+            <h2>About us</h2>
+            <p>Everything you about to know</p>
+        </div>
+    );
+};
+
+export default About;
